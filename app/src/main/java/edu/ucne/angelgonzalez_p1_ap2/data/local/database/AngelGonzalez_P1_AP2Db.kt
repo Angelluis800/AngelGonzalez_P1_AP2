b5515473cc0ba1032f81f6b077d333d144060a2a
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2025-01-29 20:47:10 -0400

    Creando el Database

---
+++ app/src/main/java/edu/ucne/angelgonzalez_p1_ap2/data/local/database/AngelGonzalez_P1_AP2Db.kt
@@ -0,0 +1,14 @@
+package edu.ucne.angelgonzalez_p1_ap2.data.local.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [
+
+    ],
+    version = 1,
+    exportSchema = false
+)abstract class AngelGonzalez_P1_AP2Db : RoomDatabase(){
+
+}
