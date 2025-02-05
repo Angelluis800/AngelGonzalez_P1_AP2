1187f5e82b10da553938c3d8f1553173d4cc4c89
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2025-02-05 19:46:52 -0400

    Cambiando nombre del ViewModel

--- app/src/main/java/edu/ucne/angelgonzalez_p1_ap2/presentation/entidad/SistemaViewModel.kt
+++ app/src/main/java/edu/ucne/angelgonzalez_p1_ap2/presentation/entidad/SistemaViewModel.kt
@@ -12,7 +12,7 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class EntidadViewModel @Inject constructor(
+class SistemaViewModel @Inject constructor(
     private val sistemaRepository: SistemaRepository
 ) : ViewModel(){
     private val _uiState = MutableStateFlow(SistemaUiState())
