7b716b1e35b9e5f8ec01ac9beb910a12de5a7e4c
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2025-02-05 18:48:09 -0400

    Agregando la anotación @Dao en el Dao

--- app/src/main/java/edu/ucne/angelgonzalez_p1_ap2/data/local/dao/SistemaDao.kt
+++ app/src/main/java/edu/ucne/angelgonzalez_p1_ap2/data/local/dao/SistemaDao.kt
@@ -1,11 +1,13 @@
 package edu.ucne.angelgonzalez_p1_ap2.data.local.dao
 
+import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Query
 import androidx.room.Upsert
 import edu.ucne.angelgonzalez_p1_ap2.data.local.entity.SistemaEntity
 import kotlinx.coroutines.flow.Flow
 
+@Dao
 interface SistemaDao {
 
     @Upsert()
