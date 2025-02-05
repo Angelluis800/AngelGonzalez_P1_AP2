ce38649b8e9380861071d8aaede9b8df3df2fe4b
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2025-02-05 19:02:31 -0400

    Cambiando a que pueda ser nullable el errorMessage

--- app/src/main/java/edu/ucne/angelgonzalez_p1_ap2/presentation/entidad/SistemaUiState.kt
+++ app/src/main/java/edu/ucne/angelgonzalez_p1_ap2/presentation/entidad/SistemaUiState.kt
@@ -5,6 +5,6 @@ import edu.ucne.angelgonzalez_p1_ap2.data.local.entity.SistemaEntity
 data class SistemaUiState(
     val sistemaId: Int? = null,
     val nombre: String = "",
-    val errorMessage: String = "",
+    val errorMessage: String? = null,
     val sistemas: List<SistemaEntity> = emptyList()
 )
