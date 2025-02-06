07c6f9e5b40770b4586f0cbb666640bb516c45b1
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2025-02-05 20:34:14 -0400

    Agregando nueva propiedad en la uiState

--- app/src/main/java/edu/ucne/angelgonzalez_p1_ap2/presentation/entidad/SistemaUiState.kt
+++ app/src/main/java/edu/ucne/angelgonzalez_p1_ap2/presentation/entidad/SistemaUiState.kt
@@ -5,6 +5,7 @@ import edu.ucne.angelgonzalez_p1_ap2.data.local.entity.SistemaEntity
 data class SistemaUiState(
     val sistemaId: Int? = null,
     val nombre: String = "",
+    val precio: String = "",
     val errorMessage: String? = null,
     val sistemas: List<SistemaEntity> = emptyList()
 )
