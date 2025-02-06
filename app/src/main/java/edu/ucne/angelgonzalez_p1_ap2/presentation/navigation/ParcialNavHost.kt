33db27a28f812898940c2768efa86f506a1c8847
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2025-02-05 20:32:21 -0400

    Agregando al Navhost la pantalla Sistema

--- app/src/main/java/edu/ucne/angelgonzalez_p1_ap2/presentation/navigation/ParcialNavHost.kt
+++ app/src/main/java/edu/ucne/angelgonzalez_p1_ap2/presentation/navigation/ParcialNavHost.kt
@@ -4,7 +4,9 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
 import edu.ucne.angelgonzalez_p1_ap2.presentation.entidad.SistemaListScreen
+import edu.ucne.angelgonzalez_p1_ap2.presentation.entidad.SistemaScreen
 
 @Composable
 
@@ -27,6 +29,16 @@ fun ParcialNavHost(
                     navHostController.navigate(Screen.Sistema(sistemaId = sistemaId))
                 }
             )
+        }
+        composable<Screen.Sistema> {
+            val sistemaId = it.toRoute<Screen.Sistema>().sistemaId
+            SistemaScreen(
+                sistemaId = sistemaId,
+                goBackToList = {
+                    navHostController.navigateUp()
+                }
+            )
+
         }
     }
 }
